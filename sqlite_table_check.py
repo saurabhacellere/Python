a3709e3478dbbf32b56da2f6a47b87fd4beb980f
saurabhacellere <saurabh.patil@acellere.com>
2019-09-29 18:34:16 +0530

    "commitMessage : fix pep8 error"

--- sqlite_table_check.py
+++ sqlite_table_check.py
@@ -13,35 +13,35 @@ import sqlite3
 import sys
 import os
 
-dropbox     = os.getenv("dropbox")
-config      = os.getenv("my_config")
-dbfile      = ("Databases\jarvis.db")
-listfile    = ("sqlite_master_table.lst")
-master_db   = os.path.join(dropbox, dbfile)
-config_file = os.path.join(config, listfile)
-tablelist   = open(config_file,'r');
+dropbox= os.getenv("dropbox")
+config=os.getenv("my_config")
+dbfile=("Databases\jarvis.db")
+listfile=("sqlite_master_table.lst")
+master_db=os.path.join(dropbox, dbfile)
+config_file=os.path.join(config, listfile)
+tablelist=open(config_file,'r');
 
-conn   = sqlite3.connect(master_db)
+conn = sqlite3.connect(master_db)
 cursor = conn.cursor()
 cursor.execute('SELECT SQLITE_VERSION()')
 data = cursor.fetchone()
 
 if str(data) == "(u'3.6.21',)":
-  print ("\nCurrently " + master_db + " is on SQLite version: %s" % data + " - OK -\n")
+    print ("\nCurrently " + master_db + " is on SQLite version: %s" % data + " - OK -\n")
 else:
-  print ("\nDB On different version than master version - !!!!! \n")
+    print ("\nDB On different version than master version - !!!!! \n")
 conn.close()
 
 print ("\nCheckling " + master_db + " against " + config_file + "\n")
 
 for table in tablelist.readlines():
-  conn = sqlite3.connect(master_db)
-  cursor = conn.cursor()
-  cursor.execute("select count(*) from sqlite_master where name = ?",(table.strip(), ))
-  res = cursor.fetchone()
+    conn = sqlite3.connect(master_db)
+    cursor = conn.cursor()
+    cursor.execute("select count(*) from sqlite_master where name = ?",(table.strip(), ))
+    res = cursor.fetchone()
     
   if (res[0]):
-    print ('[+] Table : ' + table.strip() + ' exists [+]')
+      print ('[+] Table : ' + table.strip() + ' exists [+]')
   else:
-    print ('[-] Table : ' + table.strip() + '  does not exist [-]')
+      print ('[-] Table : ' + table.strip() + '  does not exist [-]')
   
