c0213221b61ca3fd22f8cc4205bd30e9e47e9fb0
saurabhacellere <saurabh.patil@acellere.com>
2019-09-29 18:33:52 +0530

    "commitMessage : Latest Update_ for all varsions of interpreters"

--- create_dir_if_not_there.py
+++ create_dir_if_not_there.py
@@ -8,17 +8,12 @@
 #
 # Description   : Checks to see if a directory exists in the users home directory, if not then create it
 
-import os  # Import the OS module
-
-MESSAGE = 'The directory already exists.'
-TESTDIR = 'testdir'
+import os		# Import the OS module
 try:
-    home = os.path.expanduser("~")  # Set the variable home by expanding the user's set home directory
-    print(home)  # Print the location
+    home = os.path.expanduser("~")          # Set the variable home by expanding the users set home directory
+    print(home)                              # Print the location
 
-    if not os.path.exists(os.path.join(home, TESTDIR)):  # os.path.join() for making a full path safely
-        os.makedirs(os.path.join(home, TESTDIR))  # If not create the directory, inside their home directory
-    else:
-        print(MESSAGE)
+    if not os.path.exists(os.path.join(home, 'testdir')): # os.path.jon() for making a full path safely
+        os.makedirs(os.path.join(home, 'testdir'))        # If not create the directory, inside their home directory
 except Exception as e:
     print(e)
