ba0a6c6465b20b80d827ecb3f820aed62c22cccc
saurabhacellere <saurabh.patil@acellere.com>
2019-09-29 18:32:53 +0530

    "commitMessage : Removed system argument and removed the first row multiplication table"

--- Print_List_of_Even_Numbers.py
+++ Print_List_of_Even_Numbers.py
@@ -1,19 +1,8 @@
 # INPUT NUMBER OF EVEN NUMBERS
-def print_error_messages():
-  print("Invalid number, please enter a Non-negative number!")
-  exit()
 
-
-try:
-  n=int(input('Amount: '))
-except ValueError:
-  print_error_messages()
-
+n=int(input())
 start=0
 
-if n < 0:
-  print_error_messages()
-
-for i in range(n):
+for i range(n):
   print(start)
   start+=2
