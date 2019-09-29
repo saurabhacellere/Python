fcf61f6326c778aea8b17a5195cf478c212053b1
saurabhacellere <saurabh.patil@acellere.com>
2019-09-29 18:35:41 +0530

    "commitMessage : add 1 unittest to the project"

--- osinfo.py
+++ osinfo.py
@@ -1,48 +1,33 @@
 # Script Name		: osinfo.py
-# Authors		: {'geekcomputers': 'Craig Richards', 'dmahugh': 'Doug Mahugh','rutvik1010':'Rutvik Narayana Nadimpally','y12uc231': 'Satyapriya Krishna', 'minto4644':'Mohit Kumar'}
-# Created		: 5th April 2012
-# Last Modified	        : July 19 2016
-# Version		: 1.0
+# Author				: Craig Richards
+# Created				: 5th April 2012
+# Last Modified		: 
+# Version				: 1.0
 
-# Modification 1	: Changed the profile to list again. Order is important. Everytime we run script we don't want to see different ordering.
-# Modification 2        : Fixed the AttributeError checking for all properties. Using hasttr().
-# Modification 3        : Removed ': ' from properties inside profile.
+# Modifications		: 
 
+# Description			: Displays some information about the OS you are running this script on
 
-# Description		: Displays some information about the OS you are running this script on
-
-import platform as pl
+import platform
 
 profile = [
-        'architecture',
-        'linux_distribution',
-        'mac_ver',
-        'machine',
-        'node',
-        'platform',
-        'processor',
-        'python_build',
-        'python_compiler',
-        'python_version',
-        'release',
-        'system',
-        'uname',
-        'version',
-    ]
-
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
-for key in profile:
-    if hasattr(pl, key):
-        print(key + bcolors.BOLD + ": " + str(getattr(pl, key)()) + bcolors.ENDC)
-
+platform.architecture(),
+platform.dist(),
+platform.libc_ver(),
+platform.mac_ver(),
+platform.machine(),
+platform.node(),
+platform.platform(),
+platform.processor(),
+platform.python_build(),
+platform.python_compiler(),
+platform.python_version(),
+platform.release(),
+platform.system(),
+platform.uname(),
+platform.version(),
+]
+i=1
+for item in profile:
+  print '#',i,' ',item
+  i=i+1;
