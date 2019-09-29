d7c8442ad053f2f70c576a9c1a98fc629e3138e8
saurabhacellere <saurabh.patil@acellere.com>
2019-09-29 18:32:23 +0530

    "commitMessage : [GH-494]  Url change in weather scrapper"

--- Print_List_of_Odd_Numbers.py
+++ Print_List_of_Odd_Numbers.py
@@ -1,18 +1,7 @@
 # INPUT NUMBER OF ODD NUMBERS
-def print_error_messages():
-  print("Invalid number, please enter a Non-negative number!")
-  exit()
 
-
-try:
-  n=int(input('Amount: '))
-except ValueError:
-  print_error_messages()
-
+n=int(input('Amount: '))
 start=1
-
-if n < 0:
-  print_error_messages()
 
 for i in range(n):
   print(start)
